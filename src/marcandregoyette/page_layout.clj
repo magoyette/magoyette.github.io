555b1dba08bff7bacb0b17d7b70fd0208df8e58e
Marc-André Goyette <goyette.marcandre@gmail.com>
2016-01-17 16:07:20 -0500

    Remove Google Analytics

--- src/marcandregoyette/page_layout.clj
+++ src/marcandregoyette/page_layout.clj
@@ -85,15 +85,6 @@
    [:div#posts-container]
    [:div (footer)]])
 
-(def google-analytics-code
-  "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];
-  a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-46742437-1', 'auto');
-    ga('send', 'pageview');")
-
 (def disqus-code
   "var disqus_shortname = 'marcandregoyette';
   (function() {
@@ -116,6 +107,5 @@
                       (menu)
                       (post-grid)
                       [:div.additional-scripts]
-                      (hiccup-element/javascript-tag google-analytics-code)
                       (hiccup-element/javascript-tag disqus-code)
                       [:div.disqus-config]]))

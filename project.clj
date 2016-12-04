b8388e2d822b6ed0aa10af9d5c5a23313c7f5577
Marc-André Goyette <goyette.marcandre@gmail.com>
2016-12-03 21:49:01 -0500

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -6,14 +6,14 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.xml "0.0.8"]
                  [org.clojure/tools.namespace "0.2.11"]
-                 [clj-time "0.12.0"]
+                 [clj-time "0.12.2"]
                  [clygments "0.1.1"]
                  [enlive "1.1.6"]
                  [garden "1.3.2"]
                  [hiccup "1.0.5"]
                  [me.raynes/cegdown "0.1.1"]
                  [me.raynes/fs "1.4.6"]
-                 [optimus "0.18.5"]
+                 [optimus "0.19.0"]
                  [ring "1.5.0"]
                  [stasis "2.3.0"]]
   :ring {:handler marcandregoyette.core/app}
@@ -33,7 +33,7 @@
               "src/marcandregoyette/feed.clj"
               "-d" "dist/source" "-f" "index.html"]]}
   :profiles {:dev
-             {:dependencies [[prone "1.1.1"]]
+             {:dependencies [[prone "1.1.4"]]
               :plugins [[michaelblume/lein-marginalia "0.9.0"]
                         [lein-ring "0.9.7" :exclusions [org.clojure/clojure]]]
               :ring {:stacktrace-middleware prone.middleware/wrap-exceptions}}})

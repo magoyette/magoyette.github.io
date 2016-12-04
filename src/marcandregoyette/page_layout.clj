b8de5327f5f4f49c9cd4b0b71895a0daa437b275
Marc-André Goyette <goyette.marcandre@gmail.com>
2016-12-03 22:37:41 -0500

    Fix W3 HTML checker error for X-UA-Compatible

--- src/marcandregoyette/page_layout.clj
+++ src/marcandregoyette/page_layout.clj
@@ -7,7 +7,7 @@
 (defn- include-meta []
   (seq [[:meta {:charset "utf-8"}]
         [:meta {:http-equiv "X-UA-Compatible"
-                :content "IE=edge,chrome=1"}]
+                :content "IE=edge"}]
         [:meta {:name "viewport"
                 :content "width=device-width, initial-scale=1.0"}]
         [:meta {:name "author"

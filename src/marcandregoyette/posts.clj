ca205aa9e4ca7f774b0292b832dceb39d446ee39
Marc-André Goyette <goyette.marcandre@gmail.com>
2016-12-07 22:38:36 -0500

    Add Post and PostMetadata records, improve posts code

--- src/marcandregoyette/posts.clj
+++ src/marcandregoyette/posts.clj
@@ -6,8 +6,18 @@
             [net.cgrand.enlive-html :as enlive]
             [stasis.core :as stasis]))
 
-(defn- remove-meta [page]
-  (string/replace page #"(?is)^---(.*?)---" ""))
+;; Metadata that describe the post.
+(defrecord PostMetadata [title date lang category tags])
+
+;; Blog post.
+(defrecord Post [^PostMetadata metadata content])
+
+(def post-edn-header-regex #"(?is)^---(.*?)---")
+
+(defn- remove-post-metadata
+  "Remove the metadata header from the content of a post."
+  [post-content]
+  (string/replace post-content post-edn-header-regex ""))
 
 (def ^:private pegdown-options
   [:autolinks ; urls are considered as links
@@ -15,30 +25,44 @@
    :hardwraps ; alternate handling of new lines
    :strikethrough]) ; ~~text~~
 
-(defn- parse-markdown [content]
+(defn- markdown->html
+  "Convert the provided markdown content to HTML."
+  [content]
   (cegdown/to-html content pegdown-options))
 
-(defn- extract-metadata [post]
-  (edn/read-string (->> post
-                        (re-seq #"(?is)^---(.*?)---")
-                        first
-                        second)))
+(defn- read-post-metadata
+  "Retrieve the metadata of a post from its content."
+  [post]
+  (edn/read-string
+   (->> post
+        (re-seq post-edn-header-regex)
+        first
+        second)))
 
-(defn insert-post-title [post-content post-title]
+(defn insert-post-title
+  "Insert the title of a post as a h2 header before its content."
+  [post-content post-title]
   (str (string/join (enlive/emit* (enlive/html [:h2.ui.large.header post-title])))
        post-content))
 
-(defn- transform-content [post-title post-content]
+(defn- generate-post-content-html
+  "Generate the html from the markdown content of a post. The edn metadata of
+  the post is removed, the title of the post is added and the code blocks are
+  highlighted."
+  [post-title post-content]
   (-> post-content
-      remove-meta
-      parse-markdown
+      remove-post-metadata
+      markdown->html
       (insert-post-title post-title)
       highlight/highlight-code-blocks))
 
-(defn build-post-map [post]
-  (let [metadata (extract-metadata post)]
-    {:metadata metadata
-     :content (transform-content (:title metadata) post)}))
+(defn- make-post
+  "Build a Post from the contents of a post markdown file."
+  [post]
+  (let [metadata (read-post-metadata post)]
+    (map->Post
+     {:metadata (map->PostMetadata metadata)
+      :content (generate-post-content-html (:title metadata) post)})))
 
 (defn- transform-path [export-path path file-extension-regex]
   (str export-path (string/replace path file-extension-regex "/")))
@@ -48,4 +72,4 @@
   (let [posts (stasis/slurp-directory directory file-extension-regex)]
     (zipmap (map #(transform-path export-path % file-extension-regex)
                  (keys posts))
-            (map build-post-map (vals posts)))))
+            (map make-post (vals posts)))))

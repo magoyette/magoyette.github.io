0d44458cbbf0db601d1078cdc2483183c840211b
Marc-André Goyette <goyette.marcandre@gmail.com>
2017-01-25 20:50:08 -0500

    Update dependencies

--- project.clj
+++ project.clj
@@ -6,15 +6,15 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.xml "0.0.8"]
                  [org.clojure/tools.namespace "0.2.11"]
-                 [clj-time "0.12.2"]
+                 [clj-time "0.13.0"]
                  [clygments "0.1.1"]
                  [enlive "1.1.6"]
                  [garden "1.3.2"]
                  [hiccup "1.0.5"]
                  [me.raynes/cegdown "0.1.1"]
                  [me.raynes/fs "1.4.6"]
-                 [optimus "0.19.0"]
-                 [ring "1.5.0"]
+                 [optimus "0.19.1"]
+                 [ring "1.5.1"]
                  [stasis "2.3.0"]]
   :ring {:handler marcandregoyette.core/app}
   :aliases {"export"

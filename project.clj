615b70e563b0492f8080b17f15da5ae74aab73bf
magoyette <goyette.marcandre@gmail.com>
2018-11-07 23:16:25 -0500

    Update Semantic UI and Clojure dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject marcandregoyette "0.11.1"
+(defproject marcandregoyette "0.12.0"
   :description "Personal website of Marc-Andr\u00E9 Goyette (https://marcandregoyette.com)."
   :url "https://marcandregoyette.com"
   :license {:name "Eclipse Public License"
@@ -6,15 +6,15 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/data.xml "0.0.8"]
                  [org.clojure/tools.namespace "0.2.11"]
-                 [clj-time "0.14.4"]
+                 [clj-time "0.15.1"]
                  [clygments "1.0.0"]
-                 [com.vladsch.flexmark/flexmark "0.34.46"]
+                 [com.vladsch.flexmark/flexmark "0.34.56"]
                  [enlive "1.1.6"]
                  [garden "1.3.6"]
                  [hiccup "1.0.5"]
                  [me.raynes/fs "1.4.6"]
-                 [optimus "0.20.1"]
-                 [ring "1.6.3"]
+                 [optimus "0.20.2"]
+                 [ring "1.7.1"]
                  [stasis "2.3.0"]]
   :ring {:handler marcandregoyette.core/app}
   :aliases {"build-semantic" ["shell" "./build-semantic"]
@@ -38,10 +38,10 @@
                       ["shell" "./deploy-to-github-pages"]]
             "start" ["do" ["ring" "server"]]}
   :profiles {:dev
-             {:dependencies [[prone "1.6.0"]]
+             {:dependencies [[prone "1.6.1"]]
               :plugins [[lein-marginalia "0.9.1"]
                         [lein-npm "0.6.2"]
                         [lein-ring "0.12.4" :exclusions [org.clojure/clojure]]
                         [lein-shell "0.5.0"]]
               :ring {:stacktrace-middleware prone.middleware/wrap-exceptions}}}
-  :npm {:dependencies [[semantic-ui "2.3.3"]]})
+  :npm {:dependencies [[semantic-ui "2.4.2"]]})

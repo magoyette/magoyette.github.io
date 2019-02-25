6df6279f46eb714815b628e10a4b79a74cd23b99
magoyette <goyette.marcandre@gmail.com>
2019-02-24 19:57:03 -0500

    Remove unused dependency on tools.namespace

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject marcandregoyette "0.16.3"
+(defproject marcandregoyette "0.16.4"
   :description "Personal website of Marc-Andr\u00E9 Goyette (https://marcandregoyette.com)."
   :url "https://marcandregoyette.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.xml "0.0.8"]
-                 [org.clojure/tools.namespace "0.2.11"]
                  [org.clojure/tools.reader "1.3.2"]
                  [clj-time "0.15.1"]
                  [clygments "1.0.0"]

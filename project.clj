6d1b81af7ad747ec6b2f56dc16f36c0a10d7b246
magoyette <goyette.marcandre@gmail.com>
2019-03-08 18:20:39 -0500

    Fix outdated docstrings

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject marcandregoyette "0.21.0"
+(defproject marcandregoyette "0.21.1"
   :description "Personal website of Marc-Andr\u00E9 Goyette (https://marcandregoyette.com)."
   :url "https://marcandregoyette.com"
   :license {:name "Eclipse Public License"

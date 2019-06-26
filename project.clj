e505a49aac3ad085b8c8b0ec4acbe348e4d6f54b
magoyette <goyette.marcandre@gmail.com>
2019-06-25 22:08:56 -0400

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -1,18 +1,18 @@
-(defproject marcandregoyette "0.25.0"
+(defproject marcandregoyette "0.26.0"
   :description "Personal website of Marc-Andr\u00E9 Goyette (https://marcandregoyette.com)."
   :url "https://marcandregoyette.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.xml "0.0.8"]
                  [org.clojure/tools.reader "1.3.2"]
                  [clj-commons/fs "1.5.0"]
-                 [com.vladsch.flexmark/flexmark "0.42.6"]
-                 [com.vladsch.flexmark/flexmark-ext-footnotes "0.42.6"]
-                 [com.vladsch.flexmark/flexmark-ext-tables "0.42.6"]
+                 [com.vladsch.flexmark/flexmark "0.42.12"]
+                 [com.vladsch.flexmark/flexmark-ext-footnotes "0.42.12"]
+                 [com.vladsch.flexmark/flexmark-ext-tables "0.42.12"]
                  [enlive "1.1.6"]
                  [org.python/jython-standalone "2.7.1"]
-                 [org.pygments/pygments "2.3.1"]
+                 [org.pygments/pygments "2.4.2"]
                  [ring "1.7.1"]
                  [rum "0.11.3" :exclusions [cljsjs/react cljsjs/react-dom sablono]]
                  [stasis "2.4.0"]
@@ -29,7 +29,7 @@
                       ["export"]
                       ["shell" "./deploy-to-github-pages"]]}
   :profiles {:dev
-             {:dependencies [[prone "1.6.3"]]
+             {:dependencies [[prone "1.6.4"]]
               :plugins [[lein-ring "0.12.5"]
                         [lein-shell "0.5.0"]]
               :ring {:stacktrace-middleware prone.middleware/wrap-exceptions}}})

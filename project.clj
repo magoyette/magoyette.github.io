29ca8cd7213d03ee7b09d5bef866abc4dd10785a
magoyette <goyette.marcandre@gmail.com>
2019-10-19 13:57:25 -0400

    Upgrade Bulma and Clojure dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject marcandregoyette "0.33.3"
+(defproject marcandregoyette "0.34.0"
   :description "Personal website of Marc-Andr\u00E9 Goyette (https://marcandregoyette.com)."
   :url "https://marcandregoyette.com"
   :license {:name "Eclipse Public License"
@@ -9,16 +9,16 @@
                  [org.clojure/tools.reader "1.3.2"]
                  [ch.qos.logback/logback-classic "1.2.3"]
                  [clj-commons/fs "1.5.1"]
-                 [com.vladsch.flexmark/flexmark "0.50.32"]
-                 [com.vladsch.flexmark/flexmark-ext-footnotes "0.50.32"]
-                 [com.vladsch.flexmark/flexmark-ext-tables "0.50.32"]
+                 [com.vladsch.flexmark/flexmark "0.50.42"]
+                 [com.vladsch.flexmark/flexmark-ext-footnotes "0.50.42"]
+                 [com.vladsch.flexmark/flexmark-ext-tables "0.50.42"]
                  [enlive "1.1.6"]
                  [org.python/jython-standalone "2.7.1"]
                  [org.pygments/pygments "2.4.2"]
                  [ring "1.7.1"]
                  [rum "0.11.3" :exclusions [cljsjs/react cljsjs/react-dom sablono]]
                  [stasis "2.5.0"]
-                 [tongue "0.2.7"]]
+                 [tongue "0.2.8"]]
   :ring {:handler marcandregoyette.core/app}
   :jvm-opts ["-Dclojure.spec.check-asserts=true"]
   :aliases {"start" ["do"

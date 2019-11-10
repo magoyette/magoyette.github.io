fd4063c55389ddad5eebf43a07015585d0ff8309
magoyette <goyette.marcandre@gmail.com>
2019-11-10 10:42:52 -0500

    Add package-lock.json

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject marcandregoyette "0.35.0"
+(defproject marcandregoyette "0.35.1"
   :description "Personal website of Marc-Andr\u00E9 Goyette (https://marcandregoyette.com)."
   :url "https://marcandregoyette.com"
   :license {:name "Eclipse Public License"

980e05e7e9d64fb2d3a295a43ffe2c0ea7e0ca7f
Marc-André Goyette <goyette.marcandre@gmail.com>
2020-05-10 12:30:54 -0400

    Upgrade Clojure dependencies

--- project.clj
+++ project.clj
@@ -1,23 +1,23 @@
-(defproject marcandregoyette "0.46.1"
+(defproject marcandregoyette "0.46.2"
   :description "Personal website of Marc-Andr\u00E9 Goyette (https://marcandregoyette.com)."
   :url "https://marcandregoyette.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.xml "0.0.8"]
-                 [org.clojure/tools.logging "1.0.0"]
+                 [org.clojure/tools.logging "1.1.0"]
                  [org.clojure/tools.reader "1.3.2"]
                  [ch.qos.logback/logback-classic "1.2.3"]
-                 [clj-commons/fs "1.5.1"]
-                 [com.vladsch.flexmark/flexmark "0.61.16"]
-                 [com.vladsch.flexmark/flexmark-ext-footnotes "0.61.16"]
-                 [com.vladsch.flexmark/flexmark-ext-tables "0.61.16"]
+                 [clj-commons/fs "1.5.2"]
+                 [com.vladsch.flexmark/flexmark "0.61.26"]
+                 [com.vladsch.flexmark/flexmark-ext-footnotes "0.61.26"]
+                 [com.vladsch.flexmark/flexmark-ext-tables "0.61.26"]
                  [enlive "1.1.6"]
                  [org.python/jython-standalone "2.7.2"]
                  ;; Cannot upgrade to pygments 2.6, since it requires Python 3
                  [org.pygments/pygments "2.5.2"]
-                 [ring "1.8.0"]
-                 [rum "0.11.4" :exclusions [cljsjs/react cljsjs/react-dom sablono]]
+                 [ring "1.8.1"]
+                 [rum "0.11.5" :exclusions [cljsjs/react cljsjs/react-dom sablono]]
                  [stasis "2.5.0"]
                  [tongue "0.2.9"]]
   :ring {:handler marcandregoyette.core/app}

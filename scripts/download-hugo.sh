f2ea68632005a40ece6147d017bc87fbd1306b68
Marc-André Goyette <goyette.marcandre@gmail.com>
2022-08-11 13:25:44 -0400

    Improve script to download Hugo

--- scripts/download-hugo.sh
+++ scripts/download-hugo.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 set -e
 
-if ./hugo/hugo version | grep -q "v$1"; then
-  echo "Hugo v$1 is already installed"
+if [ -z "$1" ]; then
+  scriptName=$(basename "$0")
+  echo "Usage: $scriptName hugoVersion"
+  exit 1
+fi
+
+scriptDir=$(dirname "$0")
+
+if "$scriptDir"/../hugo/hugo version | grep -q "v$1"; then
+  echo "Hugo v$1 is already installed: $("$scriptDir"/../hugo/hugo version)"
   exit 0
 fi
 
-hugoFolder="./hugo"
+hugoFolder="$scriptDir/../hugo"
 
 mkdir -p "$hugoFolder"
 tempFile="$hugoFolder/hugoTemp"
@@ -15,6 +23,6 @@ echo "Downloading Hugo v$1"
 curl -L -o "$tempFile" "https://github.com/gohugoio/hugo/releases/download/v$1/hugo_extended_$1_Windows-64bit.zip"
 unzip -o -j "$tempFile" "hugo.exe" -d "$hugoFolder"
 
-rm $tempFile
+rm "$tempFile"
 
-echo "Hugo version v$1 has been installed"
+echo "Hugo version v$1 has been installed: $("$scriptDir"/../hugo/hugo version)"
